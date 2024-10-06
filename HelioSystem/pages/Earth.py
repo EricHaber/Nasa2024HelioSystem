e656a9c37c758e212d6b263b74c3ce1f637295b9
github9coder <optimusprinceps.traianus.dacicus@gmail.com>
2024-10-06 16:50:42 +0200

    Update Earth.py

--- HelioSystem/pages/Earth.py
+++ HelioSystem/pages/Earth.py
@@ -3,9 +3,9 @@ import numpy as np
 import random
 import time
 from openai import OpenAI
-import pyvista as pv
-from pyvista import examples
-from stpyvista import stpyvista
+import lib.pyvista as pv
+from lib.pyvista import examples
+from lib.stpyvista import stpyvista
 
 
 
